439fa777e7fd78932dce8943d477a4ac182f3222
Reena Joshi <rjoshi@veriskhealth.com>
2016-02-14 18:35:57 -0500

    Exploratory Data Analysis - Project1
    EDA Project1 committed

---
+++ plot1.R
@@ -0,0 +1,11 @@
+# Initializing
+dt=read.csv('household_power_consumption.txt',header=T, sep=';') # read the textfile which is formatted as a csv
+dt$Datetime = paste(as.character(dt[,1]) , dt[,2])  # merge date and time into a single column (all string variables)
+dt[,1]=as.Date(dt$Date,'%d/%m/%Y') # convert the Date column's datatype from string to date 
+dt = subset(dt, Date == '2007-02-01' | Date == '2007-02-02') #select only the two required days
+dt[,3] = as.numeric(as.character(dt[,3])) # convert the Global Active Power's datatype from text (interpreted as factors, hence the as.character) to numbers
+
+#plot 1
+png('plot1.png',width=480,height=480)
+hist(dt[,3],col='red',xlab='Global Active Power (kilowatts)', ylab='Frequency',main='Global Active Power')
+dev.off()
